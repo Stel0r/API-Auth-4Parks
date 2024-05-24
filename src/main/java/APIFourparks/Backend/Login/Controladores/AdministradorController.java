d42d8216ff3b27cc25292f0aba33c205c0deb6c0
Diego Gamez <65517739+Stel0r@users.noreply.github.com>
2024-05-24 17:56:11 -0500

    cambios

--- src/main/java/APIFourparks/Backend/Login/Controladores/AdministradorController.java
+++ src/main/java/APIFourparks/Backend/Login/Controladores/AdministradorController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import APIFourparks.Backend.Login.Controladores.Repositorios.AdminRespository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/administrador")
 public class AdministradorController {
 
