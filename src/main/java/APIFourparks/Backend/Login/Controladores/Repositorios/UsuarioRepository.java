af6e56310a1f41eb70dea913d96e95dd38dbe6fc
Diego Gamez <diego.felipe.gamez@gmail.com>
2024-05-26 20:42:37 -0500

    Update UsuarioRepository.java

--- src/main/java/APIFourparks/Backend/Login/Controladores/Repositorios/UsuarioRepository.java
+++ src/main/java/APIFourparks/Backend/Login/Controladores/Repositorios/UsuarioRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import APIFourparks.Backend.Login.Logica.Usuario;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario,String>{
 
-    @Modifying()
+    @Modifying
+    @Transactional
     @Query(value = "UPDATE USUARIO SET I_ESTADO = 'A',N_INTENTOS_FALLIDOS = 0 WHERE N_NOMBRE_USUARIO = ?",nativeQuery = true)
     public void desbloquear(String user);
 
